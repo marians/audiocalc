2f65bec7474304f20b4bcc9292f1854d97d6ac34
Marian Steinbach <marian@sendung.de>
2013-12-03 10:16:20 +0100

    License added

--- setup.py
+++ setup.py
@@ -2,11 +2,19 @@
 
 from distutils.core import setup
 
+try:
+    import pypandoc
+    description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    description = ''
+
 setup(name='audiocalc',
       version='0.0.4',
       description='A few audio/sound calculation utilities',
+      long_description=description,
       author='Marian Steinbach',
       author_email='marian@sendung.de',
       url='https://github.com/marians/audiocalc',
       packages=['audiocalc'],
+      license='MIT',
       requires=[])
